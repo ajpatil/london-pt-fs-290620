dce9a7b9ecc9b225779b0fcaca1551c8bc5eb673
AJINKYA PATIL <amackp@gmail.com>
2020-07-21 20:16:41 +0100

    ex23

--- module-1/23-for-in-loop-exercise/index.js
+++ module-1/23-for-in-loop-exercise/index.js
@@ -4,18 +4,40 @@
  * take object as a param and console log each property in a format
  * "Property name: {propertyName}"
  */
+ function logProperties(obj) {
+   for (prop in obj){
+     console.log(`Property name: ${prop}`)
+
+   }
+};
 
 /**
  * Exercise 2
  * Create a function {getAllValues} that takes an object
  * and returns an array of all values.
  */
+ function getAllValues(obj){
+  let objArray = [];
+  for (let elements in obj){
+    return Object.values(obj)
+  }
+
+};
 
 /**
  * Exercise 3
  * Create a function {getAllValuesAsString} that takes an object
  * and returns all values as one string with space separation.
  */
+ function getAllValuesAsString(obj) {
+   let emptyString = ""
+   for(let elements in obj) {
+     emptyString += `${obj[elements]}` + " "
+     }
+     return emptyString.trim()
+
+   }
+
 
 /**
  * Exercise 4
@@ -23,3 +45,9 @@
  * and log property and values as string in a next format:
  * 'PROPERTY: VALUE'
  */
+ function propertyValueString(obj) {
+
+  for (let prop in obj) {
+    console.log(`${prop}: ${obj[prop]}`)
+  }
+};
