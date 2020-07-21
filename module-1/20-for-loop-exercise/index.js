f644f8a803554e9fc80bc92e39562cc3c05b79be
AJINKYA PATIL <amackp@gmail.com>
2020-07-21 19:47:44 +0100

    ex20

--- module-1/20-for-loop-exercise/index.js
+++ module-1/20-for-loop-exercise/index.js
@@ -1,6 +1,6 @@
-const myName = "John"; // SET YOUR NAME
+const myName = "AJ"; // SET YOUR NAME
 
-const visitors = ["Alex", "Mike", "Sam"]; // Add at least 5 names
+const visitors = ["Alex", "Mike", "Sam","Ben","Joe"]; // Add at least 5 names
 
 /**
  * Exercise 1
@@ -9,6 +9,12 @@ const visitors = ["Alex", "Mike", "Sam"]; // Add at least 5 names
  * will greet all people from "visitors" for each person you should
  * console.log "Hi {name}, my name is {myName}!"
  */
+ function greetVisitors () {
+   for (let visitor=0; visitor < visitors.length; visitor++) {
+     console.log('Hi '+ visitors[visitor] + ' my name is ' + myName +'!')
+   }
+ }
+ // greetVisitors()
 
 // ========================
 const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
@@ -19,6 +25,18 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
  * given item in {numbers}.
  * If the item is not present, return a string "Item not found"
  */
+ function getElementIndex(number) {
+   for (let item = 0; item < numbers.length; item++) {
+     if (numbers[item] === number) {
+       return item;
+     }
+
+   }
+   return 'Item not found';
+ }
+
+
+
 
 // ========================
 
@@ -27,6 +45,14 @@ const numbers = [2, 123, 13, 1, 6, 84, 23, 12, 45]; // DON'T TOUCH THIS LINE :)
  * Create a function {total} that will return sum of all numbers
  * from {numbers} array
  */
+ function total() {
+   let sumOf = 0;
+   for (let num=0; num < numbers.length; num++) {
+     sumOf += numbers[num]
+
+   }
+   return sumOf;
+ }
 
 // ========================
 
@@ -37,6 +63,14 @@ const numbersForIndexes = [6, 2, 16, 23, 84, 21, 9, 3]; // DON'T TOUCH THIS LINE
  * Create a function {addIndex} that takes {numbersForIndexes} array,
  * and returns a new array which includes each number + it's index
  */
+ function addIndex () {
+   let numWithIndex = [];
+   for (let arr =0; arr < numbersForIndexes.length; arr++) {
+     numWithIndex.push(numbersForIndexes[arr]+ arr);
+   }
+   return numWithIndex;
+}
+
 
 const forLowestNumber = [5, 234, 96, 34, -34, 0, 23]; // DON'T TOUCH THIS LINE :)
 /**
@@ -44,3 +78,12 @@ const forLowestNumber = [5, 234, 96, 34, -34, 0, 23]; // DON'T TOUCH THIS LINE :
  *  create function {lowestNumber} which will return the lowest integer
  * from {forLowestNumber}
  */
+ function lowestNumber() {
+   let smallestInteger = 0;
+   for (let smallestNum in forLowestNumber){
+     if(forLowestNumber[smallestNum] < smallestInteger) {
+       smallestInteger = forLowestNumber[smallestNum]
+     }
+   } return smallestInteger
+
+ };
