611641c6580a08611694d895828282440c5b53d2
AJINKYA PATIL <amackp@gmail.com>
2020-07-21 20:15:22 +0100

    ex22

--- module-1/22-for-of-loop-exercise/index.js
+++ module-1/22-for-of-loop-exercise/index.js
@@ -10,6 +10,14 @@
  * a new array with these integers. If there is no integers it should return
  * empty array.
  */
+ function getIntegersOnly(arr) {
+    let numArray = [];
+    for(let element of arr){
+      if( Number.isInteger(element)) {
+        numArray.push(element)
+      }
+    } return numArray
+  }
 
 /**
  * Exercise 2
@@ -17,6 +25,15 @@
  * and returns a new array with even numbers only, if there is no even
  * integers it should return an empty array
  */
+ function getEvenNumbers(evenArr) {
+   let evenNumbers = [];
+   for (let num of evenArr){
+     if(num % 2 === 0){
+      evenNumbers.push(num)
+     }
+   }
+   return evenNumbers
+ }
 
 /**
  * Exercise 3
@@ -26,6 +43,17 @@
  * a new array with even numbers only, if there is no even integers it
  * should return an empty array
  */
+ function getEvenNumbersFromMixedArray(mixArray) {
+  let evenArray = [];
+  for (let elements of mixArray) {
+    if(Number.isInteger(elements) && elements % 2 === 0){
+      evenArray.push(elements)
+    }
+
+  } return evenArray
+
+};
+
 
 /**
  * Exercise 4
@@ -33,6 +61,17 @@
  * and returns a new array with odd numbers only, if there is no odd
  * integers it should return an empty array
  */
+ function getOddNumbers(numArray) {
+    let oddArray = [];
+  for (let element of numArray) {
+    if(Number.isInteger(element) && element % 2 !== 0){
+      oddArray.push(element)
+    }
+
+  } return oddArray
+
+};
+
 
 /**
  * Exercise 5
@@ -43,3 +82,15 @@
  * If your param is [1,2,3,4,5] the result should be
  * [2,1,4,3,6]
  */
+ function evenOddTransform(numSwitch){
+  let switchedArray = [];
+  for (let num of numSwitch) {
+    if (num % 2 === 0){
+      switchedArray.push(num - 1)
+    }
+    else {
+      switchedArray.push(num + 1)
+    }
+  }
+  return switchedArray
+};
