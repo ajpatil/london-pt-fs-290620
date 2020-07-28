df0e0a139ea306af808ce7f9d444da5474448517
AJINKYA PATIL <amackp@gmail.com>
2020-07-28 22:00:36 +0100

    ex24 done

--- module-1/24-DIY-loops-js-exercise/index.js
+++ module-1/24-DIY-loops-js-exercise/index.js
@@ -38,38 +38,38 @@ const dataTypes = [
  * }
  */
 
-// const sortedData = (data) => {
-//   let result = {
-//     strings: [],
-//     integers: [],
-//     floats: [],
-//     arrays: [],
-//     objects: [],
-//   };
+const sortedData = (data) => {
+  let result = {
+    strings: [],
+    integers: [],
+    floats: [],
+    arrays: [],
+    objects: [],
+  };
 
-//   for (let el of data) {
-//     switch (true) {
-//       case typeof el === "string":
-//         result.strings.push(el);
-//         break;
-//       case Number.isInteger(el) === true:
-//         result.integers.push(el);
-//         break;
-//       case Number(el) === el && el % 1 !== 0:
-//         result.floats.push(el);
-//         break;
-//       case Array.isArray(el) === true:
-//         result.arrays.push(el);
-//         break;
-//       case typeof el === "object":
-//         result.objects.push(el);
-//         break;
-//       default:
-//         console.log("undefined element");
-//     }
-//   }
-//   return result;
-// };
+  for (let el of data) {
+    switch (true) {
+      case typeof el === "string":
+        result.strings.push(el);
+        break;
+      case Number.isInteger(el) === true:
+        result.integers.push(el);
+        break;
+      case Number(el) === el && el % 1 !== 0:
+        result.floats.push(el);
+        break;
+      case Array.isArray(el) === true:
+        result.arrays.push(el);
+        break;
+      case typeof el === "object":
+        result.objects.push(el);
+        break;
+      default:
+        console.log("undefined element");
+    }
+  }
+  return result;
+};
 
 const numbers = [4, 3, 2, 6, 1, 7];
 
@@ -93,7 +93,6 @@ const multipliedByNextNumber = function (arr) {
   return finalArray;
 };
 
-
 /**
  * Exercise 3
  *
@@ -106,31 +105,16 @@ const multipliedByNextNumber = function (arr) {
  * ex: multipliedEvenNumbers([4,3,6,8,5,7])
  * result: [24, 3, 48, 16, 5, 7]
  */
-
-// const findNextEvenEl = (arr, index) => {
-//   for (let i = index; i < arr.length; i++) {
-//     if (arr[i] % 2 === 0) {
-//       return arr[i];
-//     }
-//   }
-
-//   return 2;
-// };
-
-// const multipliedEvenNumbers = (nums) => {
-//   const result = [];
-
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] % 2 === 0) {
-//       const nextEvenNum = findNextEvenEl(nums, i + 1);
-//       result.push(nums[i] * nextEvenNum);
-//     } else {
-//       result.push(nums[i]);
-//     }
-//   }
-
-//   return result;
-// };
+const multipliedEvenNumbers = (array) => {
+  return array.map((item, key, arr) => {
+    if (item % 2) {
+      return item;
+    } else {
+      let piece = arr.slice(key + 1);
+      return item * (piece.find((i) => i % 2 === 0) || 2);
+    }
+  });
+};
 
 /**
  * Exercise 4
@@ -145,30 +129,16 @@ const multipliedByNextNumber = function (arr) {
  * result: [4, 15, 6, 8, 35, 7]
  */
 
-// const findNextOddEl = (arr, index) => {
-//   for (let i = index; i < arr.length; i++) {
-//     if (arr[i] % 2) {
-//       return arr[i];
-//     }
-//   }
-
-//   return 1;
-// };
-
-// const multipliedOddNumbers = (nums) => {
-//   const result = [];
-
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] % 2) {
-//       const nextEvenNum = findNextOddEl(nums, i + 1);
-//       result.push(nums[i] * nextEvenNum);
-//     } else {
-//       result.push(nums[i]);
-//     }
-//   }
-
-//   return result;
-// };
+const multipliedOddNumbers = (array) => {
+  return array.map((item, key, arr) => {
+    if (item % 2) {
+      let piece = arr.slice(key + 1);
+      return item * (piece.find((i) => i % 2 === 1) || 1);
+    } else {
+      return item;
+    }
+  });
+};
 
 /**
  * Exercise 5
@@ -181,13 +151,12 @@ const multipliedByNextNumber = function (arr) {
  * else return original array.
  */
 
-// const multipliedEvenOddNumbers = (nums, type) => {
-//   switch (type) {
-//     case "even":
-//       return multipliedEvenNumbers(nums);
-//     case "odd":
-//       return multipliedOddNumbers(nums);
-//     default:
-//       return nums;
-//   }
-// };
+const multipliedEvenOddNumbers = (nums, type) => {
+  if (type === "even") {
+    return multipliedEvenNumbers(nums);
+  } else if (type === "odd") {
+    return multipliedOddNumbers(nums);
+  } else {
+    return nums;
+  }
+};
--- module-1/25-functions-default-arguments-exercise/index.js
+++ module-1/25-functions-default-arguments-exercise/index.js
@@ -1,29 +1,35 @@
 /**
  * Exercise 1
- * 
+ *
  * create a function {sayHi} which takes param
- * {name} and return 'Hi, {name}!' if you don't pass any 
- * param it should use default value for name which 
+ * {name} and return 'Hi, {name}!' if you don't pass any
+ * param it should use default value for name which
  * should be 'incognito'
  */
+const sayHi = (name = "incognito") => `Hi, ${name}!`;
 
 /**
  * Exercise 2
- * 
+ *
  * create a function {multiply} which takes 2 numbers
  * as params, multiply them and return a result.
- * 
- * It you don't pass 1 or both params it should use 1 as 
+ *
+ * It you don't pass 1 or both params it should use 1 as
  * a default value/values
  */
-
+const multiply = (num1 = 1, num2 = 1) => num1 * num2;
 /**
  * Exercise 3
  * create a function {logElements} that takes array as a param
- * by looping through it log every element. If you don't 
- * pass any array, default value of your param should be 
+ * by looping through it log every element. If you don't
+ * pass any array, default value of your param should be
  * array that contains only one element - string with value "Nothing to log"
  */
+const logElements = (arr = ["Nothing to log"]) => {
+  for (let element of arr) {
+    console.log(element);
+  }
+};
 
 /**
  * Exercise 4
@@ -32,3 +38,8 @@
  * Default value of the param should be object with property name,
  * and value "John Smith"
  */
+const getUserName = (obj = { name: "John Smith" }) => {
+  for (let keys in obj) {
+    return obj[keys];
+  }
+};
